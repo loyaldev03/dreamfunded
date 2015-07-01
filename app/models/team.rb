95ee1b95d0f46e7b7a80874a4323273320f2e5e7
minkim0706 <min.kim@datastax.com>
2015-06-30 23:49:15 -0700

    fixed team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,6 @@
 class Team < ActiveRecord::Base
+	validates :full_name, presence:true
+	validates :image_name, presence:true
+	validates :description, presence:true
+	validates :full_bio, presence:true
 end
--- db/migrate/20150630053943_create_teams.rb
+++ db/migrate/20150630053943_create_teams.rb
@@ -1,8 +1,11 @@
 class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
-
-      t.timestamps
+    	t.column :full_name, :string
+    	t.column :image_name, :string
+    	t.column :description, :string
+    	#full bio will have redundant contents; will think later whether to fix
+    	t.column :full_bio, :string
     end
   end
 end
