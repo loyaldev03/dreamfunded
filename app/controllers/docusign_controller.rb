06ce6a1238d66af11675e20d90df18691b077c8c
tomas0706 <tomas76@stanford.edu>
2015-07-18 15:20:51 -0700

    Added JQUERY to rails

--- app/controllers/docusign_controller.rb
+++ app/controllers/docusign_controller.rb
@@ -5,12 +5,10 @@ class DocusignController < ApplicationController
   # or
   # @envelope_response = client.create_envelope_from_template
   def embedded_signing
-    client = DocusignRest::Client.new
-    @url = client.get_recipient_view(
-      envelope_id: @envelope_response["envelopeId"],
-      name: current_user.display_name,
-      email: current_user.email,
-      return_url: "http://localhost:3000/docusign_response"
-    )
+
   end  
+
+  def get_envelope
+  end 
+
 end
