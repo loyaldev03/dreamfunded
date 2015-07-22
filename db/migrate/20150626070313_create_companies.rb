5b9ef4259ba6182068f302f44976ba08c0ed7805
Youdong Zhang <youdongz@berkeley.edu>
2015-07-22 11:57:25 -0700

    bigger

--- db/migrate/20150626070313_create_companies.rb
+++ db/migrate/20150626070313_create_companies.rb
@@ -6,6 +6,10 @@ class CreateCompanies < ActiveRecord::Migration
     	t.column :description,:string
     	t.column :image_file_name, :string
     	t.column :invested_amount, :integer
+        t.column :website_link, :string
+        t.column :video_link, :string
+        t.column :goal_amount, :integer
+        t.column :status, :integer
     	t.timestamps
     end
   end
