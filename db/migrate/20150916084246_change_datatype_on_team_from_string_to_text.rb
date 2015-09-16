df716d8ce9fc1517a15e3de89028ed8127b91cf8
tomas0706 <tomas76@stanford.edu>
2015-09-16 01:58:41 -0700

    added change file to db migration

--- db/migrate/20150916084246_change_datatype_on_team_from_string_to_text.rb
+++ db/migrate/20150916084246_change_datatype_on_team_from_string_to_text.rb
@@ -1,7 +1,8 @@
 class ChangeDatatypeOnTeamFromStringToText < ActiveRecord::Migration
   def change
 	  def up
-	    change_column :team, :fullbio, :text
+	    change_column :teams, :fullbio, :text
+	    change_column :teams, :summary, :text
 	  end
   end
 end
