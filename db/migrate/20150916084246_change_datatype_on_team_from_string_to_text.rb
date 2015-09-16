209d1fea913618ea2e38ab0bda1b1c5fcab76aca
tomas0706 <tomas76@stanford.edu>
2015-09-16 01:51:37 -0700

    same as previous

--- db/migrate/20150916084246_change_datatype_on_team_from_string_to_text.rb
+++ db/migrate/20150916084246_change_datatype_on_team_from_string_to_text.rb
@@ -1,11 +1,7 @@
 class ChangeDatatypeOnTeamFromStringToText < ActiveRecord::Migration
   def change
 	  def up
-	    change_column :team, :fullbio, :text, :limit => nil
-	  end
-
-	  def down
-	    change_column :team, :fullbio, :string
+	    change_column :team, :fullbio, :text
 	  end
   end
 end
