368e14c95d1cf9197efd6214606b31dc95196628
alexander <alexandr.larionov88@gmail.com>
2015-10-08 14:58:30 -0700

    updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.delivery_method = :smtp
-    config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
+    config.action_mailer.default_url_options = { host: 'immense-sea-5165.herokuapp.com' }
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.default :charset => "utf-8"
