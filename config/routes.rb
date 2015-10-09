8064662e1d10f40a17c49fa56c4da4c252c3a46a
alexander <alexandr.larionov88@gmail.com>
2015-10-09 15:11:04 -0700

    redirect link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :documents
 
-  match "/diversity-tech-angels-earn-wings/" => redirect("https://dreamfundedsf.wpengine.com/diversity-tech-angels-earn-wings/")
+  match "/diversity-tech-angels-earn-wings/" => redirect("https://dreamfundedsf.wpengine.com/diversity-tech-angels-earn-wings/"), via: 'get'
 
   get 'payment/index'
 
