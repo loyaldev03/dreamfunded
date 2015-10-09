46b568450bf5e4e7d467975d6de1c1e489a4ee92
alexander <alexandr.larionov88@gmail.com>
2015-10-09 15:08:54 -0700

    redirect link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :documents
 
+  match "/diversity-tech-angels-earn-wings/" => redirect("https://dreamfundedsf.wpengine.com/diversity-tech-angels-earn-wings/")
 
   get 'payment/index'
 
