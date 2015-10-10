a9c9347c5a6c5eb30074334dfd74fb9fd942479b
alexander <alexandr.larionov88@gmail.com>
2015-10-09 18:08:52 -0700

    changed fieldtype for Team table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009223501) do
+ActiveRecord::Schema.define(version: 20151010010645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,12 +68,16 @@ ActiveRecord::Schema.define(version: 20151009223501) do
   end
 
   create_table "news", force: true do |t|
-    t.string   "title"
+    t.text     "title"
     t.string   "image_filename"
     t.text     "content"
-    t.string   "source"
+    t.text     "source"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "sections", force: true do |t|
