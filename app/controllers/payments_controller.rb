c938c829e4f9803cc612aed5a85321a631327879
alexander <alexandr.larionov88@gmail.com>
2015-10-18 11:34:07 -0700

    payment test

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,52 +5,52 @@ class PaymentsController < ApplicationController
 	end
 
   def payment
-    tran=UmTransaction.new
+    # tran=UmTransaction.new
 
-    # Merchants Source key must be generated within the console
-    tran.key="azIZnB64RLfnc7yFhWbidTGTgkdq5p36"
+    # # Merchants Source key must be generated within the console
+    # tran.key="azIZnB64RLfnc7yFhWbidTGTgkdq5p36"
 
-    # Send request to sandbox server not production.  Make sure to comment or remove this line before
-    #  putting your code into production
-    #tran.usesandbox=true
+    # # Send request to sandbox server not production.  Make sure to comment or remove this line before
+    # #  putting your code into production
+    # #tran.usesandbox=true
 
-    # tran.card="4111111111111111"
-    # tran.exp="0919"
-    tran.command="check:sale"
-    tran.amount= params[:amout]
-    tran.invoice="1234"
-    #tran.cardholder="Test T Jones"
-    #tran.street="1234 Main Street"
-    #tran.zip="90036"
-    tran.description="Online Order"
-    #tran.cvv2="435"
-    # tran.custom3="testest"
-    #tran.pin="1234"
-    tran.ip = request.remote_ip
-    tran.dlnum = 'F2127533'
-    tran.dlstate = 'CA'
-    tran.cardholder = params[:name]
-    tran.routing = params[:routing]
-    tran.account = params[:account]
+    # # tran.card="4111111111111111"
+    # # tran.exp="0919"
+    # tran.command="check:sale"
+    # tran.amount= params[:amout]
+    # tran.invoice="1234"
+    # #tran.cardholder="Test T Jones"
+    # #tran.street="1234 Main Street"
+    # #tran.zip="90036"
+    # tran.description="Online Order"
+    # #tran.cvv2="435"
+    # # tran.custom3="testest"
+    # #tran.pin="1234"
+    # tran.ip = request.remote_ip
+    # tran.dlnum = 'F2127533'
+    # tran.dlstate = 'CA'
+    # tran.cardholder = params[:name]
+    # tran.routing = params[:routing]
+    # tran.account = params[:account]
 
 
-    $stdout.flush
+    # $stdout.flush
 
-    tran.process
-    flash[:message] = tran.error
-    if tran.resultcode.to_s=="A"
-    then
-      flash[:message] = tran.result
-      p "Full result #{tran.result}"
-      p "Authcode:  #{tran.authcode} "
-      p "AVS Result: #{tran.avs_result} "
-      p "Cvv2 Result: #{tran.cvv2_result} "
-    else
+    # tran.process
+    # flash[:message] = tran.error
+    # if tran.resultcode.to_s=="A"
+    # then
+    #   flash[:message] = tran.result
+    #   p "Full result #{tran.result}"
+    #   p "Authcode:  #{tran.authcode} "
+    #   p "AVS Result: #{tran.avs_result} "
+    #   p "Cvv2 Result: #{tran.cvv2_result} "
+    # else
 
-      p "Card Declined #{tran.result} "
-      p "Reason: #{tran.error}"
-    end
-    redirect_to payment_path
+    #   p "Card Declined #{tran.result} "
+    #   p "Reason: #{tran.error}"
+    # end
+    # redirect_to payment_path
   end
 
   private
