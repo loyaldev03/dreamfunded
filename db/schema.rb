ddc59eb18c06a55172885ceb9d54750503a8cf7c
alexander <alexandr.larionov88@gmail.com>
2015-12-19 14:14:24 -0800

    wording chagne

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213001435) do
+ActiveRecord::Schema.define(version: 20151219220515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,13 @@ ActiveRecord::Schema.define(version: 20151213001435) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "guests", force: true do |t|
+    t.string   "email"
+    t.string   "newsletter_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "investments", force: true do |t|
