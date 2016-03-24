89a7b91a3a0379cc2ed6cac5a9ea4935a39ca770
alexander <alexandr.larionov88@gmail.com>
2016-03-23 19:27:00 -0700

    active admin setup

---
+++ app/admin/news.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register News do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+  index do
+    column  "title"
+    column  "source"
+    column  "created_at"
+    column  "updated_at"
+  end
+
+end
