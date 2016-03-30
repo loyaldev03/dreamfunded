cdd423bdc16cefbb4223fe3f4433c49393b0dbff
alexander <alexandr.larionov88@gmail.com>
2016-03-30 16:58:41 -0700

    event image added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -34,7 +34,7 @@ class EventsController < InheritedResources::Base
   private
 
     def event_params
-      params.require(:event).permit(:name, :location, :description, :date, :link)
+      params.require(:event).permit(:name, :location, :description, :date, :link, :image)
     end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330175112) do
+ActiveRecord::Schema.define(version: 20160330233058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,10 @@ ActiveRecord::Schema.define(version: 20160330175112) do
     t.string   "link"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "founders", force: true do |t|
