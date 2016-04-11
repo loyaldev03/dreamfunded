1f350185ca9c114a381a3066cd36ea602a85fe8f
alexander <alexandr.larionov88@gmail.com>
2016-04-11 12:29:46 -0700

    exchange button

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -4,7 +4,7 @@ class SellersController < ApplicationController
     @seller = user_session
     @shares = @seller.liquidate_shares
     @company = @seller.liquidate_shares.first.company
-    @bids = Bid.where(company_id: @company.id)
+    @bids = Bid.where(company_id: @company.id).order("created_at DESC")
   end
 
 end
