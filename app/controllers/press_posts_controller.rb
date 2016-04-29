a6c3df83915d5f5f814252112755a59b0b3532a3
alexander <alexandr.larionov88@gmail.com>
2016-04-28 19:38:51 -0700

    press news styles

--- app/controllers/press_posts_controller.rb
+++ app/controllers/press_posts_controller.rb
@@ -8,9 +8,20 @@ class PressPostsController < ApplicationController
     @press_post = PressPost.new
   end
 
+  def create
+    @press_post = PressPost.new(press_post_params)
+
+      if @press_post.save
+        redirect_to press_posts_path, notice: 'Member was successfully created.'
+      else
+        render :new
+      end
+
+  end
+
   private
 
   def press_post_params
-      params.require(:press_post).permit(:date, :name, :url, :source)
+      params.require(:press_post).permit(:date, :name, :url, :image, :quote)
   end
 end
