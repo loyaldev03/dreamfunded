82f4efa013d0e0d0d754607aa989ce7134f02c26
alexander <alexandr.larionov88@gmail.com>
2016-05-10 11:27:34 -0700

    password update in admin panle

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -10,7 +10,7 @@ ActiveAdmin.register User do
     end
   end
 
-permit_params :first_name, :last_name, :login, :email, :authority, :salt, :password_digest, :confirmed, :invested_amount, :phone
+permit_params :first_name, :last_name, :login, :email, :authority, :salt, :password, :password_confirmation, :confirmed, :invested_amount, :phone
 
   index do
     column  "first_name"
@@ -26,7 +26,21 @@ permit_params :first_name, :last_name, :login, :email, :authority, :salt, :passw
     actions
   end
 
-
-
+  form do |f|
+    f.inputs 'User Details' do
+      f.input   :first_name
+      f.input   :last_name
+      f.input   :login
+      f.input   :email
+      f.input   :authority
+      f.input   :salt
+      f.input   :password
+      f.input   :password_confirmation
+      f.input   :confirmed
+      f.input   :invested_amount
+      f.input   :phone
+    end
+    f.actions
+  end
 
 end
