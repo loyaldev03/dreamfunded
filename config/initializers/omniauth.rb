de30681112f44d979aa3f78661cb7884167c9d7b
alexander <alexandr.larionov88@gmail.com>
2016-05-13 16:55:28 -0700

    yahoo omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '53258587093-kl5m6bi61j835409ncebrhr9dtvmt0se.apps.googleusercontent.com', 'zuN6_MdtvUkOlqKfyL9sL6tF',  scope: ['contacts','userinfo.email','userinfo.profile', 'https://www.google.com/m8/feeds/']
   provider :linkedin, "7516b9z1t4s1e2", "DI175eyKuk2I67Hj"
   provider :facebook, "1711154149164475", "77ca9854afc0e1f4c4b76ed4b2401459"
+  provider :yahoo_oauth2, "dj0yJmk9ZW1XSENiYzNHMkFVJmQ9WVdrOVdHOVplRmxLTnpnbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD00Ng--", "dd234123852df0e19bf355a73718afa37c05435b", name: 'yahoo'
 end
 
 OmniAuth.config.on_failure = Proc.new do |env|
--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,9 @@ Rails.application.routes.draw do
   get 'auth/facebook/callback', to:'omniauth_callbacks#google_oauth2'
   post 'auth/facebook/callback', to:'omniauth_callbacks#google_oauth2'
 
+  get 'auth/yahoo_oauth2/callback', to:'omniauth_callbacks#google_oauth2'
+  post 'auth/yahoo_oauth2/callback', to:'omniauth_callbacks#google_oauth2'
+
   # B I D D I N G   S Y S T E M
   get 'bid/:id', to: "bids#bid"
   get 'sellers_bids', to: "bids#sellers_bids"
