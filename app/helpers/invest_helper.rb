17ca692974e761711df5c0feb30877225fad9b8d
alexander <alexandr.larionov88@gmail.com>
2016-06-03 15:47:07 -0700

    register as investor

--- app/helpers/invest_helper.rb
+++ app/helpers/invest_helper.rb
@@ -3,4 +3,21 @@ module InvestHelper
   def company_price(company)
     number_with_precision(company.suggested_target_price, :precision => 2)
   end
+
+  def maximum_investment(investor)
+
+
+    income = investor.annual_income
+    net_worth = investor.new_worth
+
+    min = income < net_worth ? income : net_worth
+
+    if min < 100000
+      allowed_to_invest = min * 0.05
+      allowed_to_invest = allowed_to_invest > 2000 ? allowed_to_invest : 2000
+    elsif min > 100000
+      allowed_to_invest = min * 0.1
+    end
+    allowed_to_invest
+  end
 end
