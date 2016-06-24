a4b05033a7cd8c22673fd4d3d4dc86b62b36884e
alexander <alexandr.larionov88@gmail.com>
2016-06-24 11:32:27 -0700

    production emails changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,13 +65,13 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
  config.action_mailer.raise_delivery_errors = true
- config.action_mailer.default_url_options = { host: 'www.dreamfunded.com'}
+ config.action_mailer.default_url_options = { host: 'www.dreamfunded.vc'}
  config.action_mailer.perform_deliveries = true
  config.action_mailer.delivery_method = :smtp
  ActionMailer::Base.smtp_settings = {
    :user_name => 'dreamfunded',
    :password => 'hasin@Af6',
-   :domain => "dreamfunded.com",
+   :domain => "dreamfunded.vc",
    :address => 'smtp.sendgrid.net',
    :port => 587,
    :authentication => :plain,
