c7cb5bde055437a64dd87d09b5869dd21b1fcc8e
alexander <alexandr.larionov88@gmail.com>
2016-06-27 15:47:39 -0700

    sdf

--- db/migrate/20160627204704_create_comments.rb
+++ db/migrate/20160627204704_create_comments.rb
@@ -2,9 +2,9 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :content
-      t.integet :user_id
-      t.integet :parent_id
-      t.integet :company_id
+      t.integer :user_id
+      t.integer :parent_id
+      t.integer :company_id
       t.timestamps
     end
   end
