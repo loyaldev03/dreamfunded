1335e34357aa738bcbafa84d2e2d071ba9ddf3dd
alexander <alexandr.larionov88@gmail.com>
2016-06-29 18:26:18 -0700

    login url

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,5 +1,12 @@
 module CompaniesHelper
 	def nl2br(s)
         s.gsub(/\n/, '<br>')
-    end
+  end
+
+def nested_messages(messages)
+    messages.map do |message, sub_messages|
+      render(message) + content_tag(:div, nested_messages(sub_messages), :class => "nested_messages")
+    end.join.html_safe
+end
+
 end
