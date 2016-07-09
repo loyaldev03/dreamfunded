74b752bc427e8058cd65d57a1d24c68bcd748d39
alexander <alexandr.larionov88@gmail.com>
2016-07-08 17:33:48 -0700

    ckeditor image upload to s3

--- app/assets/javascripts/ckeditor/config_myapp.js
+++ app/assets/javascripts/ckeditor/config_myapp.js
@@ -1,5 +1,5 @@
 CKEDITOR.editorConfig = function (config) {
   // ... other configuration ...
-  config.height = '600px';
+  // config.height = '600px';
 
 }
--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -2,14 +2,14 @@ class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
                     :storage => :s3,
                     :bucket => 'dreamfunded',
-                    :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :styles => { :content => '800>', :thumb => '118x100#' },
+                    :path => "documents/:filename",
+                    :url =>':s3_domain_url',
                     :s3_protocol => :https,
                     :s3_credentials => {
                       :access_key_id => "AKIAJWDE6UJS56MXQYPQ",
                       :secret_access_key => "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"
-                    }
+                    },
+                    :styles => { :content => '800>', :thumb => '118x100#' }
 
   validates_attachment_presence :data
   validates_attachment_size :data, :less_than => 2.megabytes
