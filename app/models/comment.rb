939edef0b3902ce58b0d0362d6d5c0c547d0c961
alexander <alexandr.larionov88@gmail.com>
2016-08-08 10:36:35 -0700

    comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,17 @@
 class Comment < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :company
+    belongs_to :user
+    belongs_to :company
 
-  has_ancestry
+    has_ancestry
+
+    def comment_owner
+        if comment_belongs_to_company_owner?
+            self.company.name + "Team"
+        end
+    end
+
+    def comment_belongs_to_company_owner?
+        self.company.user == self.user
+    end
+
 end
