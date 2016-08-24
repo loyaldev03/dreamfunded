1fdbd1f2f68a46c43d6a85cdd891b2641b6a23bd
alexander <alexandr.larionov88@gmail.com>
2016-08-23 17:18:40 -0700

    fonts added

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Dreamfunded
     config.generators do |g|
       g.scaffold_controller "scaffold_controller"
     end
+
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
