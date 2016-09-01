885a93d5d5fbb2ab0a471ecdbcb20fc69bdf31aa
alexander <alexandr.larionov88@gmail.com>
2016-09-01 10:26:38 -0700

    hide raised amount if 0

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,12 +1,20 @@
 module CompaniesHelper
-	def nl2br(s)
-    s.gsub(/\n/, '<br>')
-  end
+    def nl2br(s)
+        s.gsub(/\n/, '<br>')
+    end
 
-def nested_messages(messages)
-    messages.map do |message, sub_messages|
-        render(message) + content_tag(:div, nested_messages(sub_messages), :class => "nested_messages")
-    end.join.html_safe
-end
+    def nested_messages(messages)
+        messages.map do |message, sub_messages|
+            render(message) + content_tag(:div, nested_messages(sub_messages), :class => "nested_messages")
+        end.join.html_safe
+    end
+
+    def raised_amount(company)
+        if company.invested == 0
+            return 'XXX'
+        else
+            return number_to_currency company.invested, precision: 0
+        end
+    end
 
 end
