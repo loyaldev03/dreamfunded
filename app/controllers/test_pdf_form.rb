b61e144a7fbf7b0e63ac81fe3a844dc066d527d4
alexander <alexandr.larionov88@gmail.com>
2016-09-20 16:05:23 -0700

    formc multiple sec holders

--- app/controllers/test_pdf_form.rb
+++ app/controllers/test_pdf_form.rb
@@ -10,19 +10,31 @@ class TestPdfForm < FillablePdfForm
   def fill_out
 
     [ :name, :state, :address, :website, :type_of_securtity, :max_amount, :company_location_address,
-     :company_location_city, :company_location_state, :company_location_zipcode, :days, :legal_name,
+     :company_location_city, :company_location_state, :company_location_zipcode, :days, :legal_name, :dead_line,
      :position_title, :first_date, :prev_emp, :prev_title, :prev_dates, :prev_resp, :offering_purpose, :fin_condition
      ].each do |field|
       fill field, @info.send(field)
     end
+
     fill :date, @info.date_formed
-
-    fill :security_holder, @info.principal_holders.last.name
-    fill :class_of_securities, @info.principal_holders.last.securities_held
-    fill :voting_power, @info.principal_holders.last.voting_power
     fill :type_of_security, @info.type_of_securtity
-    fill :deadline, @info.dead_line
+    #fill :deadline, @info.dead_line
 
+    fill :security_holder_0, @info.principal_holders.first.try(:name)
+    fill :class_of_securities_0, @info.principal_holders.first.try(:securities_held)
+    fill :voting_power_0, @info.principal_holders.first.try(:voting_power)
+
+    fill :security_holder_1, @info.principal_holders.second.try(:name)
+    fill :class_of_securities_1, @info.principal_holders.second.try(:securities_held)
+    fill :voting_power_1, @info.principal_holders.second.try(:voting_power)
+
+    fill :security_holder_2, @info.principal_holders.third.try(:name)
+    fill :class_of_securities_2, @info.principal_holders.third.try(:securities_held)
+    fill :voting_power_2, @info.principal_holders.third.try(:voting_power)
+
+    fill :security_holder_3, @info.principal_holders.fourth.try(:name)
+    fill :class_of_securities_3, @info.principal_holders.fourth.try(:securities_held)
+    fill :voting_power_3, @info.principal_holders.fourth.try(:voting_power)
 
   end
 end
