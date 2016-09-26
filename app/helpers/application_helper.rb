4382e8a57ac3775829a9153b3f3144ef6164c2ed
alexander <alexandr.larionov88@gmail.com>
2016-09-26 13:57:25 -0700

    campaign steps validation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,7 +42,7 @@ module ApplicationHelper
 
   def all_states
     [
-      "Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho",
+      "Delaware","Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho",
       "Illinois", "Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota",
       "Mississippi","Missouri","Montana", "Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina",
       "North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania", "Rhode Island","South Carolina","South Dakota","Tennessee",
