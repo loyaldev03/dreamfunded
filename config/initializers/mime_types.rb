659d3b74aa6168a5fe3c361b0bdffda5df620626
alexander <alexandr.larionov88@gmail.com>
2016-11-17 11:14:42 -1000

    initializer for paperclip and windows csv

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,7 +1,4 @@
 # Be sure to restart your server when you modify this file.
-Paperclip.options[:content_type_mappings] = { csv: 'application/vnd.ms-excel' }
+
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
-comma_separated_values = MIME::Types["text/comma-separated-values"].first
-comma_separated_values.extensions << "csv"
-MIME::Types.index_extensions comma_separated_values
