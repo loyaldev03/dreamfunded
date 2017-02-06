bdb4882d5f4dc20769ba38e86f16259aeec8d5b1
alexander <alexandr.larionov88@gmail.com>
2017-02-06 15:50:26 -0800

    active admin filter

--- app/admin/guest.rb
+++ app/admin/guest.rb
@@ -13,7 +13,7 @@ ActiveAdmin.register Guest  do
   end
 
 # config.filters = false
-filter :company, as: 'select', collection: proc { Company.order(:name) }
+filter :company, as: 'select', collection: proc { Company.all.pluck(:name) }
 
 permit_params  :user_id, :email, :name, :created_at, :updated_at
 
