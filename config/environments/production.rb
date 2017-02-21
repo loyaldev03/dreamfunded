4e46ed2c39aca259390b4d191436469667dcaa62
alexander <alexandr.larionov88@gmail.com>
2017-02-21 15:24:54 -0800

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
  config.action_mailer.delivery_method = :smtp
  ActionMailer::Base.smtp_settings = {
    :user_name => 'dreamfunded',
-   :password => 'hasin@Af6',
+   :password => 'kokin8$$',
    :domain => "dreamfunded.com",
    :address => 'smtp.sendgrid.net',
    :port => 587,
